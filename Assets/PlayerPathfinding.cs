9ad3f6085b903c7693fb4f45f6a7135c234f50d3
Shae Brown <t-shbro@microsoft.com>
2018-07-24 16:03:02 -0700

    Add pathfinding scene

---
+++ Assets/PlayerPathfinding.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerPathfinding : MonoBehaviour {
+    private NavMeshAgent agent;
+    private GameObject[] goals;
+
+	// Use this for initialization
+	void Start () {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        goals = GameObject.FindGameObjectsWithTag("Goal");
+	}
+
+    /*
+     *  Movable walls should be an array of walls excluding the wall that was just disabled.
+     */
+    public void onWallEnabled(GameObject enableWall, Queue<GameObject> movableWalls)
+    {
+        enableWall.GetComponent<MeshRenderer>().enabled = true;
+        if (isPathToGoal())
+        {
+            Debug.Log("The wall enabled did not block the path");
+            return;
+        }
+        Debug.Log("The wall disabled did block the path");
+        disableWallToMakePath(enableWall, movableWalls);
+    }
+
+    private void disableWallToMakePath(GameObject enableWall, Queue<GameObject> movableWalls)
+    {
+        if (movableWalls.Count == 0)
+        {
+            Debug.LogError("There is no other wall to disable, disabling wall");
+            enableWall.GetComponent<MeshRenderer>().enabled = false;
+        }
+        GameObject wall = movableWalls.Dequeue();
+        wall.GetComponent<MeshRenderer>().enabled = false;
+        if (isPathToGoal())
+        {
+            Debug.Log("Found a wall to disable");
+        } else
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+            disableWallToMakePath(enableWall, movableWalls);
+        }
+    }
+	
+	private bool isPathToGoal()
+    {
+        NavMeshPath path = new NavMeshPath();
+        foreach (var goal in goals)
+        {
+            agent.CalculatePath(goal.transform.position, path);
+            if (path.status == NavMeshPathStatus.PathComplete)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- Assets/Scripts/WallHelper.cs
+++ Assets/Scripts/WallHelper.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WallHelper : MonoBehaviour {
+    public bool isTransparent;
     private bool isActive = false;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        this.ToggleWall(!isTransparent);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,9 +21,24 @@ public class WallHelper : MonoBehaviour {
 
     }
 
-    public void ToggleWall()
+    public void ToggleWall(bool toggleOn)
     {
+        Color color = gameObject.GetComponent<Renderer>().material.color;
+        if (toggleOn)
+        {
+            color.a = 1.0f;
+            gameObject.GetComponent<NavMeshObstacle>().enabled = true;
+        } else
+        {
+            color.a = 0.5f;
+            gameObject.GetComponent<NavMeshObstacle>().enabled = false;
+        }
+        gameObject.GetComponent<Renderer>().material.color = color;
+    }
 
+    public bool isToggled()
+    {
+        return gameObject.GetComponent<NavMeshObstacle>().enabled;
     }
 
     public bool getIsActive()
